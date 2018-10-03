9d516385a42d99588851beb41de5b15965f3c06f
Shinko Sasaki <lab@shinko.info>
2018-10-03 22:25:56 +0900

    Update GPU BenchMark Score Exporter.user.js
    UL Benchmarksに対応するため書き換え

--- GPU BenchMark Score Exporter.user.js
+++ GPU BenchMark Score Exporter.user.js
@@ -1,40 +1,83 @@
-// ==UserScript==
-// @name         GPU BenchMark Score Exporter
-// @namespace    Shinko Lab.
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @include      https://www.futuremark.com/hardware/gpu*
-// @match        https://www.futuremark.com/hardware/gpu*
-// @grant        none
-// ==/UserScript==
+var table = document.getElementById('productTable');
+//console.log(table);
 
-var product = document.querySelectorAll("[class=productnameBold]");
-var score = document.querySelectorAll("[class=barHolder]");
-var plength = product.length;
-var slength = score.length;
+var rawtr = table.rows;
+//console.log(rawtr);
 
-for (var i = 0; i < plength; i++) {
-  var p = product[i].innerText;
-  var s = score[i].innerText;
-  console.log(p);
-  console.log(s);
-}
+var trlength = rawtr.length
+//console.log(trlength);
 
-var fetch = null;
-if (plength == slength) {
-    fetch = plength;
-    console.log("取得に成功しました。\n取得数は " + fetch + " 件です。");
-} else if (plength != slength) {
-    fetch = 0;
-    console.log("取得に失敗しました。");
-} else {
-    console.log("エラーが発生しました。");
-}
+var pArrAll = new Array();
+for (var i = 0; i < trlength; i++) {
+  Rank = rawtr[i].cells[0].innerText;
+  pdName = rawtr[i].cells[1].childNodes[1].innerText;
+  //TODO:ReadRanting pdRant =
+  //pdDX = rawtr[i].cells[1].childNodes[5].innerText;
+  //pdPrice = rawtr[i].cells[2].childNodes[1].innerText;
+  pdScoreRaw = rawtr[i].cells[3].childNodes[1].innerText;
+  pdScore = pdScoreRaw.replace(/\r?\n/g, "");
+  //pdVfM = rawtr[i].cells[4].childNodes[1].innerText;
+  pdPopRaw = rawtr[i].cells[5].childNodes[1].innerText;
+  pdPop = pdPopRaw.replace(/\r?\n/g, "");
 
-(function() {
-    'use strict';
+  //console.log(Rank);
+  //console.log(pdName);
+  //console.log(pdDX);
+  //console.log(pdPrice);
+  //console.log(pdScore);
+  //console.log(pdVfM);
+  //console.log(pdPop);
 
-    // Your code here...
+  var pArr = new Array();
+  pArr.push(Rank);
+  pArr.push(pdName);
+  pArr.push(pdScore);
+  pArr.push(pdPop);
+  //console.log(pArr);
+  pArrAll.push(pArr);
+};
+
+console.log(pArrAll);
+
+//Create CSV Format and Export by @ooooooo_q
+var downloadCsv = (function() {
+
+  var tableToCsvString = function(table) {
+    var str = '\uFEFF';
+    for (var i = 0, imax = table.length - 1; i <= imax; ++i) {
+      var row = table[i];
+      for (var j = 0, jmax = row.length - 1; j <= jmax; ++j) {
+        str += '"' + row[j].replace('"', '""') + '"';
+        if (j !== jmax) {
+          str += ',';
+        }
+      }
+      str += '\n';
+    }
+    return str;
+  };
+
+  var createDataUriFromString = function(str) {
+    return 'data:text/csv,' + encodeURIComponent(str);
+  }
+
+  var downloadDataUri = function(uri, filename) {
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = uri;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return function(table, filename) {
+    if (!filename) {
+      filename = 'output.csv';
+    }
+    var uri = createDataUriFromString(tableToCsvString(table));
+    downloadDataUri(uri, filename);
+  };
 
 })();
+
+downloadCsv(pArrAll);
